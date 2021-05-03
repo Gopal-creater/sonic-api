ecda574bfea640e0b7541d13875809a567795664
githubprabin143 <prabinkarki643@gmail.com>
2021-05-03 17:09:56 +0545

    plugin error fixed

--- src/api/thirdparty-detection/dto/create-thirdparty-detection.dto.ts
+++ src/api/thirdparty-detection/dto/create-thirdparty-detection.dto.ts
@@ -1,3 +1,3 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { ThirdpartyDetection } from '../schemas/thirdparty-detection.schema';
-export class CreateThirdpartyDetectionDto extends OmitType(ThirdpartyDetection,['id']) {}
+export class CreateThirdpartyDetectionDto extends OmitType(ThirdpartyDetection,['id','customer']) {}
