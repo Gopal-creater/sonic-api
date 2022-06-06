d8f113ff5e6aeba042a007158157d47af5774830
Prabin <prabin@arbadev.com>
2022-06-06 17:26:39 +0545

    minor bug fixes

--- dist/src/shared/pipes/parseQueryValue.pipe.js
+++ dist/src/shared/pipes/parseQueryValue.pipe.js
@@ -33,7 +33,11 @@ let ParseQueryValue = class ParseQueryValue {
         var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w;
         try {
             const { aggregateSearch, relation_filter = JSON.stringify({}) } = queries, query = __rest(queries, ["aggregateSearch", "relation_filter"]);
-            const parser = new mongoose_query_parser_1.MongooseQueryParser();
+            const parser = new mongoose_query_parser_1.MongooseQueryParser({
+                casters: {
+                    objectId: val => mongoose_utils_1.toObjectId(val)
+                }
+            });
             const relationPrefix = 'relation_';
             const queryToParse = Object.assign({ page: 1 }, query);
             var parsed = parser.parse(queryToParse);
