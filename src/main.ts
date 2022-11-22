7df776bf7350c0e1d7804de965500cc87ab27a91
Prabin <prabin@arbadev.com>
2022-11-22 21:11:59 +0545

    Cors enabled for AmazingPortal

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,49 @@ import { useContainer, Validator } from 'class-validator';
 
 global['fetch'] = require('node-fetch');
 
+var allowOriginList = [
+  //AmazingTag
+  'https://amazingportal.arba-dev.uk',
+  'http://amazingportal.arba-dev.uk',
+  'https://amazingadmin.arba-dev.uk',
+  'http://amazingadmin.arba-dev.uk',
+  'https://portal.sonicdata.com',
+  'https://admin.sonicdata.com',
+  'http://admin.sonicdata.com',
+  'https://sonicportal.arba-dev.uk',
+  'http://sonicportal.arba-dev.uk',
+  'https://sonictempportal.arba-dev.uk',
+  'https://sonicadminportal.arba-dev.uk',
+  'http://sonicadminportal.arba-dev.uk',
+  'https://sonicwpms.arba-dev.uk',
+  'http://sonicwpms.arba-dev.uk',
+  'http://fpserver.sonicdata.com',
+  'https://fpserver.sonicdata.com',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:8001',
+  'https://localhost:8001',
+  'http://localhost:8002',
+  'https://localhost:8002',
+  'http://localhost:8003',
+  'https://localhost:8003',
+  'http://localhost:8004',
+  'https://localhost:8004',
+];
+var corsOptionsDelegate = function(req, callback) {
+ var corsOptions;
+ if (allowOriginList.indexOf(req.header('Origin')) !== -1) {
+   corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+ } else {
+   corsOptions = { origin: false }; // disable CORS for this request
+ }
+ callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: true,
+    cors:corsOptionsDelegate
   });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.use(
@@ -30,45 +70,8 @@ async function bootstrap() {
   app.enableVersioning();
   const configService = app.get(ConfigService);
   // app.enableCors()
-  var allowOriginList = [
-     //AmazingTag
-     'https://amazingportal.arba-dev.uk',
-     'http://amazingportal.arba-dev.uk',
-     'https://amazingadmin.arba-dev.uk',
-     'http://amazingadmin.arba-dev.uk',
-     'https://portal.sonicdata.com',
-     'https://admin.sonicdata.com',
-     'http://admin.sonicdata.com',
-     'https://sonicportal.arba-dev.uk',
-     'http://sonicportal.arba-dev.uk',
-     'https://sonictempportal.arba-dev.uk',
-     'https://sonicadminportal.arba-dev.uk',
-     'http://sonicadminportal.arba-dev.uk',
-     'https://sonicwpms.arba-dev.uk',
-     'http://sonicwpms.arba-dev.uk',
-     'http://fpserver.sonicdata.com',
-     'https://fpserver.sonicdata.com',
-     'http://localhost:3000',
-     'https://localhost:3000',
-     'http://localhost:8001',
-     'https://localhost:8001',
-     'http://localhost:8002',
-     'https://localhost:8002',
-     'http://localhost:8003',
-     'https://localhost:8003',
-     'http://localhost:8004',
-     'https://localhost:8004',
-  ];
-  var corsOptionsDelegate = function(req, callback) {
-    var corsOptions;
-    if (allowOriginList.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-    } else {
-      corsOptions = { origin: false }; // disable CORS for this request
-    }
-    callback(null, corsOptions); // callback expects two parameters: error and options
-  };
-  app.enableCors(corsOptionsDelegate)
+
+  // app.enableCors(corsOptionsDelegate)
   // app.enableCors({
   //   origin: [
   //     //AmazingTag
